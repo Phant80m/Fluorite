96b501d28c5623bdd0d5d7b18155d5ac8c561bf2
phant80m <black.pandazayd@gmail.com>
2023-07-04 20:46:53 +1000

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set your Discord token
-export DISCORD_TOKEN="your_discord_token_here"
+export DISCORD_TOKEN="MTExNjg4NjE4NzQ5MjI0NTU4NQ.GXIkDW.yiqQfYGQ_fBr2WG6hAFQiLLqhTY68Q854pY03E"
 
 cargo build --release
 clear
